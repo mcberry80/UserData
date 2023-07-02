4006f9a13f720792b48589935b9599f4b5c57697
Michael Berry <mcberry@gmail.com>
2023-07-02 11:13:18 -0400

    Reduce duplicate/unused code

--- src/login.jsx
+++ src/login.jsx
@@ -1,17 +1,10 @@
 import PropTypes from 'prop-types';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase';
 
 function Login({ signInWithGoogle }) {
-  const [user] = useAuthState(auth);
 
   return (
     <div>
-      {user ? (
-        <button onClick={() => auth.signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-      )}
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   );
 }
